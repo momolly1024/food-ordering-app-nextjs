ad2ff2024dc3348c2bd83bc615a2ad4ff02589e7
momolly1024 <momolly1024@gmail.com>
2022-05-04 15:05:42 +0800

    update

--- components/Add.jsx
+++ components/Add.jsx
@@ -31,7 +31,7 @@ const Add = ({ setClose }) => {
         data.append('upload_preset', 'uploads')
         try {
             const uploadRes = await axios.post(
-                'https://api.cloudinary.com/v1_1/dsbyq4sj1/image/upload',
+                'https://api.cloudinary.com/v1_1/di6ufczpw/image/upload',
                 data
             )
 
